94baf09dc58305c8fa50e232c8572ee02eb3e4c6
Gomez Angeles <gca935@gmail.com>
2022-06-20 08:07:07 -0300

    Actualizacion de la busqueda

--- model/SearchModel.php
+++ model/SearchModel.php
@@ -11,23 +11,6 @@ class SearchModel
         $this->database = $database;
     }
 
-    public function getDatosPor($origen, $destino)
-    {
-
-        $query = "select distinct dd.nombre as origen, 
-                    d.nombre as destino,
-                    t.fechasalida as salida, 
-                    t.fechallegada as llegada,
-                    dd.id as idOrigen,
-                    d.id as idDestino
-            from destino d join
-            tramo t on d.id=t.destinoid join
-            destino dd on dd.id=t.origenid
-            where d.id=$destino or dd.id=$origen and t.fechasalida between curdate() and curdate() + interval 30 day";
-
-        return $this->database->query($query);
-    }
-
     public function getTramoOrigen($origen, $fecha)
     {
         $query = "select t.id as tramoIdOrigen, 
@@ -93,26 +76,6 @@ class SearchModel
         }
 
         return $resultado;
-    }
-
-
-    public function getDatos($origen, $destino, $fecha, $nivelPasajero, $cabina, $servicio)
-    {
-        $query = "select distinct dd.nombre as origen, d.nombre as destino, t.fechasalida as salida, t.fechallegada as llegada,
-		sb.nombre as servicio, c.nombre as cabina, t.precio as precio, tv.nombre as tipovuelo, nv.nombre as nivelvuelo
-        from destino d join
-        tramo t on d.id=t.destinoid join
-        destino dd on dd.id=t.origenid join
-        servicioabordo sb on  t.servicioid=sb.id join
-        equipo e on sb.id=e.id join
-        capacidadcabina cap on e.id=cap.cabinaid join
-        cabina c on cap.cabinaid=c.id join
-        tipovuelo tv on c.id=tv.id join
-        nivelvuelo nv on tv.id=nv.id
-        where dd.id=$origen and d.id=$destino and date(t.fechasalida)= \"$fecha\"
-        and c.id=$cabina and sb.id=$servicio and nv.id=$nivelPasajero";
-
-        return $this->database->query($query);
     }
 
     public function getDatosTour($fecha)
