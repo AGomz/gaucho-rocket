487545483ae8f3978f5e8e3bf9e86111ea1073c9
nico <nicolasdanielduarte1@gmail.com>
2022-06-20 20:24:42 -0300

    Actualiza la query de obtener las reservas

--- model/ReservasModel.php
+++ model/ReservasModel.php
@@ -10,18 +10,18 @@ class ReservasModel
         $this->database = $database;
     }
 
+    // TODO: Revisar query
     public function getReservas($userId)
     {
         $query = "select distinct dd.nombre as origen, dno.nombre as destino, t.fechasalida as salida, t.fechallegada as llegada,
-        sb.nombre as servicio, c.nombre as cabina, t.precio as precio, tv.nombre as tipovuelo, nv.nombre as nivelvuelo
+        sb.nombre as servicio, c.nombre as cabina, t.precio as precio, tv.nombre as tipovuelo, nv.nombre as nivelvuelo, r.pagoid
         from reserva r join
-        tramo t on r.tramoid=t.id join
+        reservatramo rt on rt.reservaid=r.id join
+        tramo t on rt.tramoid=t.id join
         destino dd on dd.id=t.origenid join
         destino dno on dno.id=t.destinoid join
-        servicioabordo sb on t.servicioid=sb.id join
-        equipo e on sb.id=e.id join
-        capacidadcabina cap on e.id=cap.cabinaid join
-        cabina c on cap.cabinaid=c.id join
+        servicioabordo sb on r.servicioid=sb.id join
+        cabina c on r.tipocabina=c.id join
         tipovuelo tv on c.id=tv.id join
         nivelvuelo nv on tv.id=nv.id
         where r.usuarioid=${userId}";
