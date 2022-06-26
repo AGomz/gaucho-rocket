b1dfc2f001332eb858ef56febb3f7ef8844cfb8b
Angeles <gca935@gmail.com>
2022-06-26 16:59:33 -0300

    Correccion en los filtros de busqueda front

--- public/js/homeFunctions.js
+++ public/js/homeFunctions.js
@@ -2,9 +2,14 @@
 
 function cambiarEstadoDeSelects(){
     let idaRadio = document.getElementById("idaRadio");
+    let tourRadio = document.getElementById("tourRadio");
+    let subRadio = document.getElementById("suborbitalRadio");
     let selectsDinamicos = document.getElementsByClassName("select");
     let radios = document.querySelectorAll(".radio");
     let opciones = document.querySelectorAll(".option-dis");
+    let opDis = document.querySelectorAll(".dis");
+    let opHab = document.querySelectorAll(".hab");
+
     let selectOrigen = document.getElementById("select-origen");
 
     /*Habilitar selects usando radio ida*/
@@ -18,16 +23,27 @@ function cambiarEstadoDeSelects(){
         }
     })
 
-    /*Deshabilitar selects usando radio tour y suborbital*/
+    /*Mostrar las opciones de origen dependiendo si es un tour o suborbital*/
+    tourRadio.addEventListener('click', function(){
+        opciones.forEach(element => {
+            element.setAttribute("class", "hidden");
+        })
+    })
+
+    subRadio.addEventListener('click', function(){
+        opHab.forEach(element => {
+            element.setAttribute("class", "show");
+        })
+        opDis.forEach(element => {
+            element.setAttribute("class", "hidden");
+        })
+    })
+
+    /*Deshabilitar select al seleccionar radio tour y suborbital*/
     for(let i=0; i<radios.length; i++){
         radios[i].addEventListener('click', function(){
             for (let i = 0; i < selectsDinamicos.length; i++) {
                 selectsDinamicos[i].disabled = true;
-
-                opciones.forEach(element => {
-                   element.setAttribute("class", "hidden");
-                })
-
             }
         })
     }
