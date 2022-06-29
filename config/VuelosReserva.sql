e2bb07246d7b6516e0ec02fdea4ddfebe484c5c4
nico <nicolasdanielduarte1@gmail.com>
2022-06-28 22:28:15 -0300

    Agrega los circuitos borrados

--- config/VuelosReserva.sql
+++ config/VuelosReserva.sql
@@ -7,7 +7,7 @@ SET @fechaLlegada1 = date_add(@fecha, INTERVAL 8 HOUR);
 SET @fechaSalida2 = date_add(@fecha, INTERVAL 16 HOUR);
 SET @fechaLlegada2 = date_add(@fecha, INTERVAL 24 HOUR);
 
-INSERT INTO Tramo (EquipoID, OrigenID, DestinoID, FechaSalida, FechaLlegada, Precio) VALUES
+INSERT INTO tramo (EquipoID, OrigenID, DestinoID, FechaSalida, FechaLlegada, Precio) VALUES
 -- Tierra / ISS 
 (12, 1, 3, @fechaSalida1, @fechaLlegada1, 500),
 (12, 3, 1, @fechaSalida2, @fechaLlegada2, 500),
@@ -18,7 +18,7 @@ SET @fechaSalida1 = date_add(@fecha, INTERVAL 8 hour);
 SET @fechaLlegada1 = date_add(@fecha, INTERVAL 16 hour);
 SET @fechaSalida2 = date_add(@fecha, INTERVAL 24 hour);
 SET @fechaLlegada2 = date_add(@fecha, INTERVAL 32 hour);
-INSERT INTO Tramo (EquipoID, OrigenID, DestinoID, FechaSalida, FechaLlegada, Precio) VALUES
+INSERT INTO tramo (EquipoID, OrigenID, DestinoID, FechaSalida, FechaLlegada, Precio) VALUES
 -- Tierra / ISS 
 (25, 2, 3, @fechaSalida1, @fechaLlegada1, 500),
 (25, 3, 2, @fechaSalida2, @fechaLlegada2, 500),
@@ -29,7 +29,7 @@ SET @fechaSalida1 = date_add(@fecha, INTERVAL 1 DAY);
 SET @fechaLlegada1 = date_add(@fecha, INTERVAL 36 DAY );
 SET @fechaSalida2 = date_add(@fecha, INTERVAL 16 DAY );
 SET @fechaLlegada2 = date_add(@fecha, INTERVAL 61 DAY );
-INSERT INTO Tramo (EquipoID, OrigenID, DestinoID, FechaSalida, FechaLlegada, Precio) VALUES
+INSERT INTO tramo (EquipoID, OrigenID, DestinoID, FechaSalida, FechaLlegada, Precio) VALUES
 -- Tour
 (33, 3, 2,  @fechaSalida1, @fechaLlegada1, 500),
 (34, 2, 3,  @fechaSalida2, @fechaLlegada2, 500);
@@ -45,7 +45,7 @@ SET @fechaLlegada2 = date_add(@fecha, INTERVAL 18 HOUR );
 SET @fechaSalida3 = date_add(@fecha, INTERVAL 20 HOUR );
 SET @fechaLlegada3 = date_add(@fecha, INTERVAL 46 HOUR );
 
-INSERT INTO Tramo (EquipoID, OrigenID, DestinoID, FechaSalida, FechaLlegada, Precio) VALUES
+INSERT INTO tramo (EquipoID, OrigenID, DestinoID, FechaSalida, FechaLlegada, Precio) VALUES
 -- ISS / orbital hotel baja aceleracion
 (41, 3, 4, @fechaSalida1, @fechaLlegada1, 500),
 -- Orbital hotel / luna - baja aceleracion
@@ -61,7 +61,7 @@ SET @fechaLlegada2 = date_add(@fecha, INTERVAL 118 HOUR );
 SET @fechaSalida3 = date_add(@fecha, INTERVAL 120 HOUR );
 SET @fechaLlegada3 = date_add(@fecha, INTERVAL 146 HOUR );
 
-INSERT INTO Tramo (EquipoID, OrigenID, DestinoID, FechaSalida, FechaLlegada, Precio) VALUES
+INSERT INTO tramo (EquipoID, OrigenID, DestinoID, FechaSalida, FechaLlegada, Precio) VALUES
 -- luna / marte- baja aceleracion
 (41, 6, 5, @fechaSalida3, @fechaLlegada3, 500),
 -- Orbital hotel / luna - baja aceleracion
@@ -77,7 +77,7 @@ SET @fechaLlegada2 = date_add(@fecha, INTERVAL 39 HOUR );
 SET @fechaSalida3 = date_add(@fecha, INTERVAL 40 HOUR );
 SET @fechaLlegada3 = date_add(@fecha, INTERVAL 52 HOUR );
 
-INSERT INTO Tramo (EquipoID, OrigenID, DestinoID, FechaSalida, FechaLlegada, Precio) VALUES
+INSERT INTO tramo (EquipoID, OrigenID, DestinoID, FechaSalida, FechaLlegada, Precio) VALUES
 -- tramo corto
 -- ISS / orbital hotel alta aceleracion
 (28, 3, 4, @fechaSalida1, @fechaLlegada1, 500),
@@ -94,7 +94,7 @@ SET @fechaLlegada2 = date_add(@fecha, INTERVAL 139 HOUR );
 SET @fechaSalida3 = date_add(@fecha, INTERVAL 140 HOUR );
 SET @fechaLlegada3 = date_add(@fecha, INTERVAL 152 HOUR );
 
-INSERT INTO Tramo (EquipoID, OrigenID, DestinoID, FechaSalida, FechaLlegada, Precio) VALUES
+INSERT INTO tramo (EquipoID, OrigenID, DestinoID, FechaSalida, FechaLlegada, Precio) VALUES
 -- luna / marte- alta aceleracion
 (28, 6, 5, @fechaSalida3, @fechaLlegada3, 500),
 -- Orbital hotel / luna - alta aceleracion
@@ -119,7 +119,7 @@ SET @fechaLlegada6 = date_add(@fecha, INTERVAL 264 HOUR );
 SET @fechaSalida7 = date_add(@fecha, INTERVAL 265 HOUR );
 SET @fechaLlegada7 = date_add(@fecha, INTERVAL 335 HOUR );
 
-INSERT INTO Tramo (EquipoID, OrigenID, DestinoID, FechaSalida, FechaLlegada, Precio) VALUES
+INSERT INTO tramo (EquipoID, OrigenID, DestinoID, FechaSalida, FechaLlegada, Precio) VALUES
 -- tramo largo
 -- ISS / luna hotel baja aceleracion
 (8, 3, 5, @fechaSalida1, @fechaLlegada1, 500),
@@ -151,7 +151,7 @@ SET @fechaLlegada6 = date_add(@fecha, INTERVAL 664 HOUR );
 SET @fechaSalida7 = date_add(@fecha, INTERVAL 665 HOUR );
 SET @fechaLlegada7 = date_add(@fecha, INTERVAL 735 HOUR );
 
-INSERT INTO Tramo (EquipoID, OrigenID, DestinoID, FechaSalida, FechaLlegada, Precio) VALUES
+INSERT INTO tramo (EquipoID, OrigenID, DestinoID, FechaSalida, FechaLlegada, Precio) VALUES
 -- tramo largo  Vuelta
 -- encendalo / titan hotel baja aceleracion
 (8, 11, 10, @fechaSalida7, @fechaLlegada4, 500),
@@ -187,7 +187,7 @@ SET @fechaLlegada6 = date_add(@fecha, INTERVAL 152 HOUR );
 SET @fechaSalida7 = date_add(@fecha, INTERVAL 152 HOUR );
 SET @fechaLlegada7 = date_add(@fecha, INTERVAL 205 HOUR );
 
-INSERT INTO Tramo (EquipoID, OrigenID, DestinoID, FechaSalida, FechaLlegada, Precio) VALUES
+INSERT INTO tramo (EquipoID, OrigenID, DestinoID, FechaSalida, FechaLlegada, Precio) VALUES
 -- tramo largo
 -- ISS / luna hotel baja aceleracion
 (8, 3, 5, @fechaSalida1, @fechaLlegada1, 500),
@@ -220,7 +220,7 @@ SET @fechaLlegada6 = date_add(@fecha, INTERVAL 452 HOUR );
 SET @fechaSalida7 = date_add(@fecha, INTERVAL 452 HOUR );
 SET @fechaLlegada7 = date_add(@fecha, INTERVAL 505 HOUR );
 
-INSERT INTO Tramo (EquipoID, OrigenID, DestinoID, FechaSalida, FechaLlegada, Precio) VALUES
+INSERT INTO tramo (EquipoID, OrigenID, DestinoID, FechaSalida, FechaLlegada, Precio) VALUES
 -- tramo largo  Vuelta
 -- encendalo / titan hotel baja aceleracion
 (8, 11, 10, @fechaSalida7, @fechaLlegada4, 500),
